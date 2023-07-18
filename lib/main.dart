4c384fc8031de41039c3100b577a2eedbf6ae8fd
flodro34 <f.derosso@gmail.com>
2023-07-18 14:35:33 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/bg_container.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: BgContainer(),
+    ),
+  ));
+}
